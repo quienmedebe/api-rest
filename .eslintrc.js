e81ee91745640ad8d9517611156deef89beb6017
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-09-08 20:41:44 +0200

    Add security plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-    "node": true,
-    "es2020": true
+  env: {
+    commonjs: true,
+    node: true,
+    es2020: true,
   },
-  "plugins": [
-    "prettier"
-  ],
-  "extends": ["eslint:recommended", "plugin:node/recommended", "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": 2020,
-    "ecmaFeatures": {
-      "impliedStrict": true
-    }
+  plugins: ['prettier', 'security'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended', 'plugin:security/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
   },
-  "settings": {},
-  "rules": {
-    "linebreak-style": ["error", "unix"],
-    "no-unused-vars": "warn",
-    "prettier/prettier": "off",
-  }
+  settings: {},
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-unused-vars': 'warn',
+    'prettier/prettier': 'off',
+  },
 };
