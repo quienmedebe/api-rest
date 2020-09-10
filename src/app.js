db910c8057b8d49bdea7309bb07a45b1bc75de24
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-09-10 20:44:22 +0200

    Implement compression capabilities

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const helmet = require('helmet');
 
 const Main = require('./modules/main');
@@ -7,6 +8,7 @@ const Main = require('./modules/main');
 const app = express();
 
 app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
