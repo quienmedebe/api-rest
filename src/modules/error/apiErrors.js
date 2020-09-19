f3c626c5641a826edaca76bedd3b87e6da49feb3
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-09-19 17:13:36 +0200

    Add centralized API error management

--- src/modules/error/apiErrors.js
+++ src/modules/error/apiErrors.js
@@ -2,5 +2,6 @@ const createErrorMessage = require('./internals/createErrorMessage');
 
 exports.TOO_MANY_REQUESTS = createErrorMessage('TOO_MANY_REQUESTS', 'Too many requests', {status: 429});
 exports.RESOURCE_NOT_FOUND = createErrorMessage('RESOURCE_NOT_FOUND', 'Resource not found', {status: 404});
+exports.INTERNAL_SERVER_ERROR = createErrorMessage('INTERNAL_SERVER_ERROR', 'Internal server error', {status: 500});
 
 module.exports = exports;
--- src/modules/logger/createHttpLogger.js
+++ src/modules/logger/createHttpLogger.js
@@ -1,15 +1,20 @@
 const winston = require('winston');
 
-const addRequestId = context =>
-  winston.format(info => {
+const addRequestId = context => {
+  return winston.format(info => {
     info.requestId = context.get('reqId');
     return info;
   });
+};
 
 function createHttpLogger(httpContext) {
   return winston.createLogger({
-    format: winston.format.combine(addRequestId(httpContext)(), winston.format.json()),
-    transports: [new winston.transports.Console()],
+    format: winston.format.combine(addRequestId(httpContext)(), winston.format.timestamp(), winston.format.json()),
+    transports: [
+      new winston.transports.Console({
+        timestamp: true,
+      }),
+    ],
   });
 }
 
