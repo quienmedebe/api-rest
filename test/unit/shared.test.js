55b5eb86e6e4e58c42939d186b3010584ff6a846
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-09-19 17:40:54 +0200

    Add tests to wrapAsync function

---
+++ test/unit/shared.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const Shared = require('../../src/modules/shared');
+
+describe('Shared module test suite', function () {
+  describe('wrapAsync test suite', function () {
+    it('given a failed promise, should execute the parameter next with the error', async function () {
+      const fn = sinon.stub().rejects();
+      const next = sinon.stub();
+      await Shared.wrapAsync(fn)(null, null, next);
+
+      sinon.assert.called(next);
+    });
+  });
+});
