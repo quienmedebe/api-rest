9fc99e9cb829d56b760b25a6e88c865521762d17
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-09-30 16:16:32 +0200

    Create account model and migration

---
+++ src/modules/database/migrations/20200930140330-accounts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('accounts', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+
+  down: async queryInterface => {
+    return await queryInterface.dropTable('accounts');
+  },
+};
