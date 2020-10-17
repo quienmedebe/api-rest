f3ab042819f7c0b9d7441f0d8d4bc35e7f50e763
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-17 16:05:15 +0200

    Add error handler test suite

---
+++ test/unit/error/handleError.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+const Errors = require('../../../src/modules/error');
+
+describe('handleError test suite', function () {
+  it('should call the logger', async function () {
+    const logger = {
+      log: sinon.stub(),
+    };
+
+    await Errors.handleError(new Error('Some error happened'), {logger});
+
+    expect(logger.log).to.have.been.calledOnce;
+  });
+});
