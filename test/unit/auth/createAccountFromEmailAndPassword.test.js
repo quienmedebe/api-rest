8a740bde1e3bb7ad8a09a6a27686f00f2d311197
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-17 15:58:14 +0200

    createAccountFromEmailAndPassword test added

---
+++ test/unit/auth/createAccountFromEmailAndPassword.test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
+const bcrypt = require('bcryptjs');
+const Auth = require('../../../src/modules/auth');
+const Database = require('../../../src/database');
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('createAccountFromEmailAndPassword', function () {
+  it('should throw an error if the email or the password are not valid', function () {
+    const resultWrongEmail = Auth.functions.createAccountFromEmailAndPassword(34, '123456');
+    const resultWrongPassword = Auth.functions.createAccountFromEmailAndPassword('test@example.com', '');
+
+    expect(resultWrongEmail).to.be.rejectedWith(Error);
+    expect(resultWrongPassword).to.be.rejectedWith(Error);
+  });
+
+  it('should call the database to get a response', async function () {
+    const databaseStub = sinon.stub(Database.functions.auth, 'createAccountFromEmailAndPassword').resolves({id: 3});
+    const bcryptStub = sinon.stub(bcrypt, 'hash').resolves('hashed_password');
+
+    const response = await Auth.functions.createAccountFromEmailAndPassword('test@example.com', '123456');
+
+    expect(databaseStub, 'Database function not called').to.have.been.calledOnce;
+    expect(response, 'Should return the record').not.to.be.undefined;
+
+    databaseStub.restore();
+    bcryptStub.restore();
+  });
+});
