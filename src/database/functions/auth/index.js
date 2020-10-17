9b3174633320cc1f73dd68c0f5cf0c21b4cabc07
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-17 12:49:23 +0200

    Update signup to the new api

--- src/database/functions/auth/index.js
+++ src/database/functions/auth/index.js
@@ -2,5 +2,6 @@ exports.getAccountFromEmail = require('./getAccountFromEmail');
 exports.getAccountFromId = require('./getAccountFromId');
 exports.createAccountFromEmailAndPassword = require('./createAccountFromEmailAndPassword');
 exports.emailExists = require('./emailExists');
+exports.ERRORS = require('./errors');
 
 module.exports = exports;
--- src/modules/shared/index.js
+++ src/modules/shared/index.js
@@ -2,5 +2,6 @@ exports.wrapAsync = require('./wrapAsync');
 exports.defs = require('./defs');
 exports.isString = require('./isString');
 exports.isNumber = require('./isNumber');
+exports.sendResponse = require('./sendResponse');
 
 module.exports = exports;
