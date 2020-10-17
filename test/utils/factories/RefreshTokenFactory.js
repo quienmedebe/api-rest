a8595749de4ebf363df6012506b9b604eaabd644
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-17 17:52:05 +0200

    Create refresh token factory

---
+++ test/utils/factories/RefreshTokenFactory.js
@@ -0,0 +1,23 @@
+const {RefreshToken} = require('../../../src/database/models');
+
+const createProperties = async (props = {}) => {
+  return {
+    expiration_datetime: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    valid: true,
+    tokens_issued: 0,
+    ...props,
+  };
+};
+
+async function RefreshTokenFactory(props = {}, json = true) {
+  const properties = await createProperties(props);
+  const instance = await RefreshToken.create(properties);
+
+  if (json) {
+    return instance.toJSON();
+  }
+
+  return instance;
+}
+
+module.exports = RefreshTokenFactory;
