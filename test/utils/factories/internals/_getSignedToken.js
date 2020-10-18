ad594127e5dda1920aa70ce3356fc8233850b95a
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-18 20:02:42 +0200

    Fix check route

--- test/utils/factories/internals/_getSignedToken.js
+++ test/utils/factories/internals/_getSignedToken.js
@@ -3,7 +3,7 @@ const Constants = require('../../constants');
 
 function _getSignedToken(accountId) {
   return async (payload = {}, options = {}) => {
-    const {secret = Constants.JWT_SECRET, expiresIn = 1000 * 60 * 5, ...jwtOptions} = options;
+    const {secret = Constants.ACCESS_TOKEN_SECRET, expiresIn = 1000 * 60 * 5, ...jwtOptions} = options;
 
     const jwtPayload = {
       id: +accountId,
