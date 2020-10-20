56ed551051ac0c1b01a05af203f1a2629d6bf7dc
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-20 19:41:05 +0200

    Create unit tests for getAccountFromEmail

--- test/integration/database/auth/emailExists.test.js
+++ test/integration/database/auth/emailExists.test.js
@@ -1,25 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-const proxyquire = require('proxyquire');
-const {makeMockModels} = require('sequelize-test-helpers');
-
-chai.use(sinonChai);
-
-const mockModels = makeMockModels({
-  EmailProvider: {
-    findOne: sinon.stub(({where}) => Promise.resolve(where.email === 'duplicated@example.com')),
-  },
-});
-
-const emailExists = proxyquire('../../../../src/database/functions/auth/emailExists.js', {
-  '../../models': mockModels,
-});
+const Utils = require('../../../utils');
+const emailExists = require('../../../../src/database/functions/auth/emailExists');
 
 describe('Database -> emailExists', function () {
+  beforeEach(async function () {
+    await Utils.scripts.truncateDB();
+  });
+
   it('should return true if the email already exists', async function () {
-    const response = await emailExists('duplicated@example.com');
+    const emailProvider = await Utils.factories.EmailProviderFactory();
+    const response = await emailExists(emailProvider.email);
 
     expect(response).to.be.true;
   });
