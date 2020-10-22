44feb15660674f3a159466c8f155f3080b06f3be
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-22 20:15:11 +0200

    Add unit tests for failure cases

--- test/unit/auth/getAccessTokenFromRefreshToken.test.js
+++ test/unit/auth/getAccessTokenFromRefreshToken.test.js
@@ -1,8 +1,13 @@
-const {expect} = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
-const Auth = require('../../../src/modules/auth');
+const randToken = require('rand-token');
 const Database = require('../../../src/database');
 const Utils = require('../../utils');
+const Auth = require('../../../src/modules/auth');
+
+chai.use(chaiAsPromised);
 
 let databaseMock;
 
@@ -15,9 +20,31 @@ describe('Auth -> getAccessTokenFromRefreshToken', function () {
     databaseMock.restore();
   });
 
-  it('should return an error if the account id is not valid');
+  it('should return an error if the account id is not valid', function () {
+    const accountId = 'not a number';
+    const refreshToken = randToken.uid(255);
+    const accessToken = Auth.functions.getAccessTokenFromRefreshToken(accountId, refreshToken, {secret: Utils.constants.ACCESS_TOKEN_SECRET});
 
-  it('should return an error if the refresh token is not valid');
+    expect(accessToken).to.be.rejected;
+  });
 
-  it('should return the access token if no error is thrown');
+  it('should return an error if the refresh token is not valid', function () {
+    const accountId = 1;
+    const refreshToken = null;
+    const accessToken = Auth.functions.getAccessTokenFromRefreshToken(accountId, refreshToken, {secret: Utils.constants.ACCESS_TOKEN_SECRET});
+
+    expect(accessToken).to.be.rejected;
+  });
+
+  it('should return an error if the refresh token is not found', async function () {
+    databaseMock.resolves(null);
+
+    const accountId = 1;
+    const refreshToken = randToken.uid(255);
+    const accessToken = await Auth.functions.getAccessTokenFromRefreshToken(accountId, refreshToken, {secret: Utils.constants.ACCESS_TOKEN_SECRET});
+
+    expect(accessToken).to.have.property('error');
+  });
+
+  it('should return the access token if there are no problems');
 });
--- test/unit/auth/getCredentials.test.js
+++ test/unit/auth/getCredentials.test.js
@@ -1,21 +1,35 @@
-const {expect} = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const Auth = require('../../../src/modules/auth');
 const Database = require('../../../src/database');
 const Utils = require('../../utils');
 
-let databaseMock;
+chai.use(chaiAsPromised);
 
-describe('Auth -> getCredentials', function () {
+const expect = chai.expect;
+
+let getActiveRefreshTokenFromAccountMock, createRefreshTokenMock, getAccessTokenFromRefreshTokenMock;
+
+describe.only('Auth -> getCredentials', function () {
   beforeEach(function () {
-    databaseMock = sinon.stub(Database.functions.auth, 'getAccountFromId');
+    getActiveRefreshTokenFromAccountMock = sinon.stub(Database.functions.auth, 'getActiveRefreshTokenFromAccount');
+    createRefreshTokenMock = sinon.stub(Database.functions.auth, 'createRefreshToken');
+    getAccessTokenFromRefreshTokenMock = sinon.stub(Auth.functions, 'getAccessTokenFromRefreshToken');
   });
 
   afterEach(function () {
-    databaseMock.restore();
+    getActiveRefreshTokenFromAccountMock.restore();
+    createRefreshTokenMock.restore();
+    getAccessTokenFromRefreshTokenMock.restore();
   });
 
-  it('should return an error if the account id is not valid');
+  it('should return an error if the account id is not valid', function () {
+    const accountId = 'not valid';
+    const credentials = Auth.functions.getCredentials(accountId);
+
+    expect(credentials).to.be.rejected;
+  });
 
   it('should return an access token from an old refresh token');
 
