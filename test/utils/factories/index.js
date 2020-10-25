9a1bb4448f1b3c3f69ce9ad324795da506c3fee5
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-10-25 15:29:49 +0100

    Pass first two tests of recover password route

--- test/utils/factories/index.js
+++ test/utils/factories/index.js
@@ -1,5 +1,6 @@
 exports.AccountFactory = require('./AccountFactory');
 exports.EmailProviderFactory = require('./EmailProviderFactory');
+exports.EmailTokenFactory = require('./EmailTokenFactory');
 exports.RefreshTokenFactory = require('./RefreshTokenFactory');
 
 module.exports = exports;
