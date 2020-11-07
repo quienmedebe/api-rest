b747f3683f28d71f1463e36c3dea2ad01f4d5ae9
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-11-07 17:46:01 +0100

    Add debt feature complete

---
+++ src/modules/debt/functions/addDebt.js
@@ -0,0 +1,43 @@
+const Ajv = require('ajv');
+const validation = require('../validation');
+const Database = require('../../../database');
+
+async function addDebt({accountId, amount, type} = {}) {
+  const ajv = new Ajv();
+  const areParametersCorrect = ajv.validate(
+    {
+      type: 'object',
+      required: ['amount', 'type'],
+      properties: {
+        accountId: validation.accountIdSchema,
+        amount: validation.amountSchema,
+        type: validation.typeSchema,
+      },
+    },
+    {accountId, amount, type}
+  );
+
+  if (!areParametersCorrect) {
+    throw new Error('Some arguments are invalid');
+  }
+
+  const newDebt = await Database.functions.debt.createDebt({
+    accountId,
+    amount,
+    type,
+  });
+
+  const jsonNewDebt = newDebt.toJSON();
+
+  delete jsonNewDebt.id;
+  const {public_id, ...attributes} = jsonNewDebt;
+
+  const parsedNewDebt = {
+    id: public_id,
+    ...attributes,
+  };
+
+  return parsedNewDebt;
+}
+
+module.exports = addDebt;
