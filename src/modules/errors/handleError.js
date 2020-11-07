864461239ac348450c7890477f55554af4558019
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-11-07 12:22:57 +0100

    Finish logger configuration

--- src/modules/errors/handleError.js
+++ src/modules/errors/handleError.js
@@ -1,5 +1,5 @@
 async function handleError(err, {logger}) {
-  logger.log('error', err.message, {name: err.name, stack: err.stack});
+  logger.log('error', err.message, {name: err.name, stack: err.stack, err});
 
   if (['development', 'test'].includes(process.env.NODE_ENV)) {
     console.log(err.stack);
