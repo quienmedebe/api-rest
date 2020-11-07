096e021bc7c7bff42b2588b14516bf0949919c27
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-11-07 16:33:11 +0100

    Add Debt model

---
+++ src/database/models/Debt.js
@@ -0,0 +1,60 @@
+'use strict';
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Debt extends Model {
+    static associate(models) {
+      Debt.belongsTo(models.Account, {
+        foreignKey: 'account_id',
+        as: 'account',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      });
+    }
+  }
+  Debt.init(
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      publicId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      account_id: {
+        type: DataTypes.BIGINT,
+        references: {
+          model: 'accounts',
+          key: 'id',
+        },
+        allowNull: true,
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      amount: {
+        type: DataTypes.DECIMAL(18, 6),
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM('DEBT', 'CREDIT'),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Debt',
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+      deletedAt: 'deleted_at',
+      underscored: true,
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Debt;
+};
