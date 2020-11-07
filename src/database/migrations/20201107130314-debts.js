b2978aa3cbfab8051f30e3ab11520ed3bd927ff3
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-11-07 16:31:36 +0100

    Add debts migration

---
+++ src/database/migrations/20201107130314-debts.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable(
+        'debts',
+        {
+          id: {
+            type: Sequelize.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          publicId: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            unique: true,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          account_id: {
+            type: Sequelize.BIGINT,
+            references: {
+              model: 'accounts',
+              key: 'id',
+            },
+            allowNull: true,
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          },
+          amount: {
+            type: Sequelize.DECIMAL(18, 6),
+            allowNull: false,
+          },
+          type: {
+            type: Sequelize.ENUM('DEBT', 'CREDIT'),
+            allowNull: false,
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.fn('NOW'),
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.fn('NOW'),
+          },
+          deleted_at: {
+            type: Sequelize.DATE,
+            allowNull: true,
+          },
+        },
+        {transaction}
+      );
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  down: async queryInterface => {
+    return queryInterface.dropTable('debts');
+  },
+};
