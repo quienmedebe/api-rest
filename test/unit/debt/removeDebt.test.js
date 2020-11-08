f00b1a75163993d18b2454cd6744d30b158ec6e2
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-11-08 12:56:14 +0100

    Add remove debt from debt module unit test

---
+++ test/unit/debt/removeDebt.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+const removeDebt = require('../../../src/modules/debt/functions/removeDebt');
+
+chai.use(chaiAsPromised);
+
+describe('Debt -> removeDebt', function () {
+  it('should throw an error if the account id, the debt id or both the amount and type are not defined', function () {
+    const invalidAccountId = removeDebt({
+      accountId: 'Not a valid id',
+      debtId: 1,
+    });
+    const invalidDebtId = removeDebt({
+      accountId: 1,
+      debtId: 'Not a valid debt id',
+    });
+
+    expect(invalidAccountId).to.be.rejected;
+    expect(invalidDebtId).to.be.rejected;
+  });
+});
